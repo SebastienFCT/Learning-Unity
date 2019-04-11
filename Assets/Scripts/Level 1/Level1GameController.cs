74b253dc5267d9fac36471f6962c99a5568f97b4
Sebastien <sebastienfct@gmail.com>
2019-04-10 23:06:11 -0500

    Adding sound manager
    - Deleted the completed folder (completed project from tutorial)
    - Added a SoundManager as a singleton with a basic method for playing a background music
    - See sounds attribution under Resources/Sounds

--- Assets/Scripts/Level 1/Level1GameController.cs
+++ Assets/Scripts/Level 1/Level1GameController.cs
@@ -6,5 +6,16 @@ public class Level1GameController : GameController {
 
     protected override void Start() {
         base.Start();
+
+        SetupSound();
+    }
+
+    // MARK: - Setup
+
+    private void SetupSound() {
+        AudioClip clip = Resources.Load("Sounds/freelance/restoration completed") as AudioClip;
+
+        Debug.Log("test" + clip.ToString());
+        SoundManager.instance.PlaySingle(clip);
     }
 }
---
+++ Assets/Scripts/Services/SoundManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System;
+
+public class SoundManager : MonoBehaviour {
+
+    public static SoundManager instance = null;
+
+    public AudioSource musicSource;
+
+    private void Awake() {
+        if (instance == null) {
+            instance = this;
+        } else if (instance != this) {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // MARK: - Methods
+
+    public void PlaySingle(AudioClip clip) {
+        musicSource.clip = clip;
+        musicSource.Play();
+    }
+
+}
