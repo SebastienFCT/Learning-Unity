add93c1a54f2c4c3f5c2767d538fdf40a663afbd
Sebastien <sebastienfct@gmail.com>
2019-04-11 23:58:10 -0500

    Started home screen

---
+++ Assets/Scripts/Home/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HomeController : MonoBehaviour {
+
+    protected void Start() {
+        SetupSound();
+    }
+
+    // MARK: - Setup
+
+    private void SetupSound() {
+        AudioClip clip = Resources.Load("Sounds/freelance/restoration completed") as AudioClip;
+
+        SoundManager.instance.PlaySingle(clip);
+    }
+}
